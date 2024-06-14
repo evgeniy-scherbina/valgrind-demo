2185a892e9f464fc87f1c6989715c9121dfb8222
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2024-06-14 14:54:12 -0400

    Commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/evgeniy-scherbina/sandbox/valgrind-demo
 
 go 1.20
+
+require github.com/linxGnu/grocksdb v1.8.13
--- main.go
+++ main.go
@@ -4,14 +4,62 @@ import "C"
 import (
 	"flag"
 	"fmt"
+	"log"
 	"time"
+
+	"github.com/linxGnu/grocksdb"
 )
+
+const pathToDB = "/tmp/rocksdb_data"
 
 func memoryLeak(n int) {
 	for i := 0; i < n; i++ {
 		var uuid *C.uchar
 		uuid = (*C.uchar)(C.malloc(16))
 		_ = uuid
+	}
+}
+
+func rocksdb(n int) {
+	bbto := grocksdb.NewDefaultBlockBasedTableOptions()
+	cache := grocksdb.NewLRUCache(1 << 30)
+	bbto.SetBlockCache(cache)
+	bbto.SetFilterPolicy(grocksdb.NewBloomFilter(10))
+
+	opts := grocksdb.NewDefaultOptions()
+	opts.SetBlockBasedTableFactory(bbto)
+	opts.SetCreateIfMissing(true)
+	opts.EnableStatistics()
+	opts.SetStatsDumpPeriodSec(1)
+	db, err := grocksdb.OpenDb(opts, pathToDB)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = db
+
+	simulateLoad(db, n)
+}
+
+func simulateLoad(db *grocksdb.DB, n int) {
+	ro := grocksdb.NewDefaultReadOptions()
+	wo := grocksdb.NewDefaultWriteOptions()
+
+	defaultKey := []byte("foo")
+	defaultValue := []byte("bar")
+
+	// if ro and wo are not used again, be sure to Close them.
+	err := db.Put(wo, defaultKey, defaultValue)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < n; i++ {
+		value, err := db.Get(ro, defaultKey)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_ = value
+		//defer value.Free()
 	}
 }
 
@@ -26,6 +74,7 @@ func main() {
 
 	fmt.Printf("start\n")
 	fmt.Printf("n: %v\n", *n)
-	memoryLeak(*n)
+	//memoryLeak(*n)
+	rocksdb(*n)
 	fmt.Printf("end\n")
 }
